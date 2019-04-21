fc851d982ca76b992ebc80d6dc2d8c9faf4d2ac1
Alexis Corrieras <alexis.corrieras@gmail.com>
2019-04-21 18:49:58 -0400

    Update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
-//create array of default friends
+//Array default friends
 
 var friends = [{
-  name: "John Torpey",
-  photo: "https://static.pexels.com/photos/343717/pexels-photo-343717.jpeg",
+  name: "Craig Adderley",
+  photo: "https://images.pexels.com/photos/1597690/pexels-photo-1597690.jpeg",
   scores: [3, 1, 4, 4, 2, 4, 5, 2, 4, 2]
 }, {
-  name: "Laura Toner",
-  photo: "https://static.pexels.com/photos/206481/pexels-photo-206481.jpeg",
+  name: "Laury Johnson",
+  photo: "https://images.pexels.com/photos/2025834/pexels-photo-2025834.jpeg",
   scores: [2, 5, 3, 4, 5, 3, 1, 3, 2, 4]
 }, {
-  name: "Bob Merad",
-  photo: "https://static.pexels.com/photos/249760/pexels-photo-249760.jpeg",
+  name: "Bruce Mars",
+  photo: "https://images.pexels.com/photos/2100183/pexels-photo-2100183.jpeg",
   scores: [4, 3, 3, 2, 1, 2, 5, 4, 2, 1]
 },{
-  name: "Amy Crosby",
-  photo: "https://static.pexels.com/photos/270308/pexels-photo-270308.jpeg",
+  name: "Marcela Dias",
+  photo: "https://images.pexels.com/photos/2146373/pexels-photo-2146373.jpeg",
   scores: [1, 2, 3, 3, 4, 1, 2, 5, 3, 3]
 }, {
-  name: "Chris Maclin",
-  photo: "https://static.pexels.com/photos/249757/pexels-photo-249757.jpeg",
+  name: "Alexander Dos Santos",
+  photo: "https://images.pexels.com/photos/1710005/pexels-photo-1710005.jpeg",
   scores: [5, 4, 5, 3, 2, 4, 2, 1, 3, 2]
 }, {
-  name: "Anna Schultz",
-  photo: "https://static.pexels.com/photos/388517/pexels-photo-388517.jpeg",
+  name: "Isabel McKinsey",
+  photo: "https://images.pexels.com/photos/2147192/pexels-photo-2147192.png",
   scores: [2, 1, 1, 2, 5, 4, 3, 3, 1, 4]
 }, {
-  name: "Jack Waller",
-  photo: "https://static.pexels.com/photos/64385/pexels-photo-64385.jpeg",
+  name: "Abhishek Gaurav",
+  photo: "https://images.pexels.com/photos/694438/pexels-photo-694438.jpeg",
   scores: [2, 2, 1, 3, 4, 2, 5, 3, 3, 4]
 }];
 
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// ===================================Dependencies========================================
+//Dependencies
 
 var express = require("express");
 var bodyParser = require("body-parser");
 
-// ==============================Set up the Express App========================================
+//Set up the Express App
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -16,12 +16,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// ==============================Require routing========================================
+//Routing
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-// =========================Starts the server to begin listening================================
+//Starts the server to begin listening
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
